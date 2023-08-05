61274c6573689a8f742dc4f24f35a6fbd5864997
YuJi-2023 <sophia.jiyu@gmail.com>
2023-08-04 19:49:10 -0700

    task 8

--- models/city.py
+++ models/city.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
 from models.base_model import BaseModel, Base
+from models.place import Place
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 
@@ -9,3 +10,4 @@ class City(BaseModel, Base):
     __tablename__ = 'cities'
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
+    places = relationship("Place", back_populates="cities", cascade="all, delete, delete-orphan")
