11c4287c5f02ec59c38fd168c8a81f80a634a139
YuJi-2023 <sophia.jiyu@gmail.com>
2023-08-15 05:00:05 -0700

    update task8

--- models/state.py
+++ models/state.py
@@ -11,8 +11,11 @@ class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
+    cities = relationship("City",
+                              back_populates="state",
+                              cascade="all, delete, delete-orphan")
 
-    if getenv("HBNB_TYPE_STORAGE") != "db":
+    #if getenv("HBNB_TYPE_STORAGE") != "db":
         @property
         def cities(self):
             """returns the list of City instances"""
@@ -24,7 +27,7 @@ class State(BaseModel, Base):
                 if self.id == city_obj.state_id:
                     city_list.append(city_obj)
             return city_list
-    else:
-        cities = relationship("City",
-                              back_populates="state",
-                              cascade="all, delete, delete-orphan")
+    #else:
+     #   cities = relationship("City",
+      #                        back_populates="state",
+       #                       cascade="all, delete, delete-orphan")
