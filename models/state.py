4542c4dc7a9ee4551aea47222b5eed7c803597ae
YuJi-2023 <sophia.jiyu@gmail.com>
2023-08-18 04:18:55 -0700

    pep8 promise last one

--- models/state.py
+++ models/state.py
@@ -17,7 +17,7 @@ class State(BaseModel, Base):
                               cascade="all, delete, delete-orphan")
     else:
         name = ""
-    
+ 
     @property
     def cities(self):
         """returns the list of City instances"""
