f7f5322b2df58d0bb1f6104462d293f1ebde4d96
YuJi-2023 <sophia.jiyu@gmail.com>
2023-08-18 04:17:48 -0700

    last update for task7

--- models/state.py
+++ models/state.py
@@ -10,22 +10,23 @@ from os import getenv
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
-    name = Column(String(128), nullable=False)
-    cities = relationship("City",
-                          backref='state',
-                          #back_populates="state",
-                          cascade="all, delete, delete-orphan")
-
     if getenv("HBNB_TYPE_STORAGE") == "db":
-        @property
-        def cities(self):
-            """returns the list of City instances"""
-            from models import storage
-            from models.city import City
-            city_list = []
-            city_dict = storage.all(City)
+        name = Column(String(128), nullable=False)
+        cities = relationship("City",
+                              back_populates="state",
+                              cascade="all, delete, delete-orphan")
+    else:
+        name = ""
+    
+    @property
+    def cities(self):
+        """returns the list of City instances"""
+        from models import storage
+        from models.city import City
+        city_list = []
+        city_dict = storage.all(City)
 
-            for city_obj in city_dict.values():
-                if self.id == city_obj.state_id:
-                    city_list.append(city_obj)
-            return city_list
+        for city_obj in city_dict.values():
+            if self.id == city_obj.state_id:
+                city_list.append(city_obj)
+        return city_list
